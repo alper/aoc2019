689c383a429448cdbde2ecaa2bf283ae56696a1a
Alper Çugun <github@alper.nl>
2019-12-29 20:10:28 +0100

    Made it work finally for the basic test case

--- src/aoc/day6.clj
+++ src/aoc/day6.clj
@@ -1,6 +1,6 @@
 (ns aoc.day6
   (:require [clojure.string :as s])
-)
+  )
 
 ; Part 1
 
@@ -47,7 +47,29 @@ K)L")
       (->branch val (map ->tree' kids))
       (->leaf val))))
 
-(defn number-of-orbits
-  ([tree & branches] (reduce + 0 (map number-of-orbits branches)))
-  ([leaf] 1)
-  ([] 0))
+; (defn number-of-orbits
+;   ([tree] (number-of-orbits tree 0))
+;   ([tree orbits] (let [[head & rest :as tree] tree, orbits orbits]
+;                    (cond 
+;                      (empty? tree) orbits
+;                      (empty? rest) orbits
+;                      (seq? rest) (reduce + (map number-of-orbits rest (inc orbits)))))))
+
+; (defn sum-of-depths
+;   [tree cur-depth] (let [[head & rest] tree]
+;                      (if (empty? rest)
+;                        cur-depth
+;                        (reduce + cur-depth (map (partial ) rest)))))
+
+
+(defn sum-of-depths
+  [branches]
+  (loop [branches branches
+         cur-depth 0
+         total-depth 0]
+    (cond
+      (empty? branches) total-depth
+      :else (recur
+             (mapcat (fn [node] (second node)) branches)
+             (inc cur-depth)
+             (+ total-depth (* (count branches) cur-depth))))))
